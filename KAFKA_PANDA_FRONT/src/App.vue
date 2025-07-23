b8b1c462c11fc011497629d733e0cc3ccc11460a
이경용 <sleekydz86@icloud.com>
2025-07-23 20:05:05 +0900

    feat: 타입스크립트로 변경

--- KAFKA_PANDA_FRONT/src/App.vue
+++ KAFKA_PANDA_FRONT/src/App.vue
@@ -1,47 +1,25 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const message = ref<string>('안녕하세요, Vue 3 + TypeScript!');
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+    <h1 class="text-4xl font-bold text-blue-600 mb-6">{{ message }}</h1>
+    <p class="text-lg text-gray-700 text-center">
+      이것은 TypeScript로 작성된 Vue 3 프로젝트의 예시입니다.
+    </p>
+    <p class="text-lg text-gray-700 text-center mt-2">
+      `package.json`에 필요한 의존성이 추가되었으며,
+      `tsconfig.json` 파일들이 설정되었습니다.
+    </p>
+    <button
+      @click="message = '메시지가 변경되었습니다!'"
+      class="mt-8 px-6 py-3 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 transition duration-300 ease-in-out"
+    >
+      메시지 변경
+    </button>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
